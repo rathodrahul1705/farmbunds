730c5463bb4ce96415382fbb9eb6d71d8ed7aa2b
Rahul Rathod <rathodrahul1705@gmail.com>
2025-10-09 07:02:49 +0530

    minor changes

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             </p>
             <div className={styles.socialLinks}>
               <a 
-                href="https://facebook.com/farmbunds" 
+                href="https://www.facebook.com/profile.php?id=61581643301918" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 aria-label="Visit our Facebook page"
@@ -35,7 +35,7 @@ const Footer = () => {
                 </svg>
               </a>
               <a 
-                href="https://instagram.com/farmbunds" 
+                href="https://www.instagram.com/farm.bunds/" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 aria-label="Follow us on Instagram"
@@ -57,7 +57,18 @@ const Footer = () => {
                 </svg>
               </a>
               <a 
-                href="https://youtube.com/farmbunds" 
+                href="https://www.linkedin.com/company/109434907/admin/dashboard/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="Follow us on LinkedIn"
+                className={styles.socialLink}
+              >
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                </svg>
+              </a>
+              <a 
+                href="https://www.youtube.com/@FarmBundsInternational" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 aria-label="Subscribe to our YouTube channel"
@@ -87,8 +98,7 @@ const Footer = () => {
               <Link to="/refunds">Refund Policy</Link>
             </div>
 
-
-          <div className={styles.linkGroup}>
+            <div className={styles.linkGroup}>
               <h4>Contact Info</h4>
               <div className={styles.contactItem}>
                 <span className={styles.contactIcon}>📧</span>
@@ -108,7 +118,6 @@ const Footer = () => {
                 </address>
               </div>
             </div>
-
           </div>
         </div>
 
